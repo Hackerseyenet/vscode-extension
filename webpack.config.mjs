23b2392127169dd7e23e20c28e60b780d4c74fda
Hackerseyenet <102905707+Hackerseyenet@users.noreply.github.com>
2026-02-16 10:48:14 +0200

    Update webpack.config.mjs

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,27 +1,27 @@
 "use strict";
 import path from "path";
+import { fileURLToPath } from "url";
 
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type WebpackConfig */
 const extensionConfig = {
-  target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-  mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  target: "node",
+  mode: "none",
 
-  entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  entry: "./src/extension.ts",
   output: {
-    // the bundle is stored in the 'out' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(path.dirname(new URL(import.meta.url).pathname), "out"),
+    path: path.resolve(__dirname, "out"),
     filename: "extension.js",
     clean: true,
     libraryTarget: "commonjs2",
   },
   externals: {
-    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vscodeignore file
+    vscode: "commonjs vscode",
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: [".ts", ".js", ".json", ".tsx", ".node"],
     extensionAlias: {
       ".js": [".ts", ".js"],
@@ -55,7 +55,7 @@ const extensionConfig = {
   },
   devtool: "nosources-source-map",
   infrastructureLogging: {
-    level: "log", // enables logging required for problem matchers
+    level: "log",
   },
 };
 export default [extensionConfig];
